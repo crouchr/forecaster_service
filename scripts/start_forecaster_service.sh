f9afae03cf9e5f2e92c8a4212e5d2f59e21d1e23
crouchr <richard.crouch@vonage.com>
2020-12-29 00:27:55 +0000

    Added initial docker compose files

--- scripts/start_forecaster_service.sh
+++ scripts/start_forecaster_service.sh
@@ -2,6 +2,5 @@
 set -e
 
 echo 'entered start_forecaster_service.sh'
-#docker pull registry:5000/forecaster_service:latest
-docker-compose -f dev-forecaster-service-docker-compose.yml up
-
+docker-compose -f dev-forecaster-service-docker-compose.yml up &
+echo 'exited start_forecaster_service.sh'
